57cb03e7ca0ff8ee98e9839ea927c01ab64ad3e7
becoo <lpb401@163.com>
2019-12-20 17:20:29 +0800

    [修改] 修改首页内容展示

--- router/main.js
+++ router/main.js
@@ -2,13 +2,43 @@
 
 const express = require('express');
 const router = express.Router();
+const Category = require('../models/Category');
+const Content = require('../models/Content');
 
 router.get('/', (req, res, next) => {
-    //console.log(req.userInfo);
-    res.render('main/index.html', {
+    let categories = [];
+    //console.log(id);
+    Category.find().then(rs => {
+        if (rs) {
+            categories = rs;
+        }else {
+            res.render('main/error', {
+                userInfo: req.userInfo,
+                message: "未找到任何分类"
+            })
+            return new Promise.reject();
+        }
+        
+        return Content.find().sort({_id: -1}).populate('category_id').then(content => {
+            if(!content) {
+                res.render('main/error' , {
+                    userInfo: req.userInfo,
+                    message: "未找到相关内容"
+                })
+                return;
+            }
+            
+            res.render('main/index' , {
+                userInfo: req.userInfo,
+                contents: content,
+                categories: categories
+            })
+        })
+    })
+    /* res.render('main/index.html', {
         //此处为模板的第二个参数，将userInfo 传递给模板以在页面中直接调用
         userInfo: req.userInfo
-    })
+    }) */
 })
 
 module.exports = router;
